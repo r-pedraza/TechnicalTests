20cac087117c7c26f8b09290c7b366c863c00d3d
Raul Pedraza <raul.pedraza@ciberexperis.es>
2017-10-18 12:55:03 +0200

    Add into interactor method to call service results news.

--- Copora360/SearchNewsResults/Interactor/SearchNewsResultsInteractor.swift
+++ Copora360/SearchNewsResults/Interactor/SearchNewsResultsInteractor.swift
@@ -1,5 +1,10 @@
 import UIKit
+import Alamofire
 
 class SearchNewsResultsInteractor: SearchNewsResultsInteractorProtocol {
-  var presenter: SearchNewsResultsPresenterProtocol?
+  var presenter: SearchNewsResultsPresenterProtocol!    
+
+    func loadNewsResults() {
+        APIClient().loadNews()
+    }
 }
