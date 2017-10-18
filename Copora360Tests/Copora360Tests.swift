82f3f7f2446146809ad59de5fa93e99e10fcc7e5
Raul Pedraza <raul.pedraza@ciberexperis.es>
2017-10-18 09:43:19 +0200

    Initial Commit

---
+++ Copora360Tests/Copora360Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Copora360Tests.swift
+//  Copora360Tests
+//
+//  Created by Raul Pedraza on 18/10/2017.
+//  Copyright © 2017 Raul Pedraza. All rights reserved.
+//
+
+import XCTest
+@testable import Copora360
+
+class Copora360Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
