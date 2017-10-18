59d34dd58e6258d9bcdc7798bdf1b75c13fa06ca
Raul Pedraza <raul.pedraza@ciberexperis.es>
2017-10-18 12:10:54 +0200

    Add necessary methods in search presenter protocol.

--- Copora360/Search/Presenter/SearchNewsPresenterProtocol.swift
+++ Copora360/Search/Presenter/SearchNewsPresenterProtocol.swift
@@ -1,4 +1,10 @@
 import UIKit
 
 protocol SearchNewsPresenterProtocol {
+    var numberOfComponents: Int { get }
+    var numberOfRows: Int { get }
+    func didTapInButton(with button: UIButton)
+    func title(forRow: Int) -> String
+    func update(with tag: Int, row: Int)
+    func goToSearchNewsResults()
 }
