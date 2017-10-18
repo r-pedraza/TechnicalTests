f1e565fc7be8d2227a1709fb55e1dbfa2dc279a5
Raul Pedraza <raul.pedraza@ciberexperis.es>
2017-10-18 12:11:33 +0200

    Setup necessary methods in search interactor protocol.

--- Copora360/Search/Interactor/SearchNewsInteractor.swift
+++ Copora360/Search/Interactor/SearchNewsInteractor.swift
@@ -1,5 +1,18 @@
 import UIKit
 
 class SearchNewsInteractor: SearchNewsInteractorProtocol {
+        
   var presenter: SearchNewsPresenterProtocol?
+    
+    var dataForFirstOption: [String] {
+        return ["mostemailed", "mostshared", "mostviewed"]
+    }
+    
+    var dataForSecondOption: [String] {
+        return ["1", "7", "30"]
+    }
+    
+    var dataForThreeOption: [String] {
+        return ["facebook", "twitter"]
+    }
 }
