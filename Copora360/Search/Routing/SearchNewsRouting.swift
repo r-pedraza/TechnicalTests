0c38eb2087ec1786ad82f69b6cc45b300965d235
Raul Pedraza <raul.pedraza@ciberexperis.es>
2017-10-18 12:54:27 +0200

    Load results news storyboard

--- Copora360/Search/Routing/SearchNewsRouting.swift
+++ Copora360/Search/Routing/SearchNewsRouting.swift
@@ -22,7 +22,9 @@ class SearchNewsRouting: SearchNewsRoutingProtocol {
     }
     
     func goToSearchNewsResults() {
-        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "SearchNewsResultsViewControllerID")
+        navigationController.pushViewController(controller, animated: true)
     }
   
 }
