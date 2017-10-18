a356e8aacfb29e61ccb98d392dcd6c089f1c1461
Raul Pedraza <raul.pedraza@ciberexperis.es>
2017-10-18 12:10:04 +0200

    Setup presenter.

--- Copora360/Search/View/SearchNewsViewProtocol.swift
+++ Copora360/Search/View/SearchNewsViewProtocol.swift
@@ -1,5 +1,7 @@
-import Foundation
+import UIKit
 
 protocol SearchNewsViewProtocol: class {
-    
+    var mainViewReference: UIView { get }
+    var pickerViewReference: UIPickerView { get }
+    var toolbarReference: UIToolbar { get }
 }
